b8a30a8ec5f4f81cca10ed4d28d38b01951e403a
Carlos Osmar Caceres <omarcaceres@live.com>
2022-04-20 18:38:55 -0300

    Reparando rutas

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const path = require('path');
-const process = require("process");
-require("dotenv").config();
- 
+const process = require('process');
 const PORT = process.env.PORT || 3000;
-//Ver CLase 38 inicio 20 min
-//Ruta elementos estaticos
-app.use(express.static(path.join(__dirname, '../public'))); //Define rutas desde el main
 
- app.listen(PORT, () => console.log(`
-    Server listen port ${PORT}
-    http://localhost:${PORT}
-    `));
-//Ruta index
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/index.html')) //Define rutas desde el main
-  })
-  
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')))
+
+
+//Enrutadores
+const indexRouter = require("./routes/indexRouter");
+
+//Middlware routers
+app.use("/",indexRouter);
+
+
+
+
+
+app.listen(PORT, () => console.log(`
+Server listen port ${PORT}
+http://localhost:${PORT}
+`))
---
+++ src/controllers/indexController.js
@@ -0,0 +1,4 @@
+module.exports = {
+    index: (req, res) => res.send("HOMEPAGE DESDE EL CONTROLLER"),
+    contact: (req, res) => res.send("CONTACTO")
+} 
